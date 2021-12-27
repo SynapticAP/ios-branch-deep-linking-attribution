2282b54c6ee6e9823226de467a44951dd489bc52
Matt Schultz <matt.schultz@synapticap.com>
2021-12-27 10:24:51 -0500

    [YRKA-161] Fix for plugin crashing when device is offline.

--- Branch-SDK/BNCPreferenceHelper.m
+++ Branch-SDK/BNCPreferenceHelper.m
@@ -15,7 +15,7 @@
 #import "NSString+Branch.h"
 
 static const NSTimeInterval DEFAULT_TIMEOUT = 5.5;
-static const NSTimeInterval DEFAULT_RETRY_INTERVAL = 0;
+static const NSTimeInterval DEFAULT_RETRY_INTERVAL = 0.1;// 0; MODIFIED for plugin crash when device offline
 static const NSInteger DEFAULT_RETRY_COUNT = 3;
 
 static NSString * const BRANCH_PREFS_FILE = @"BNCPreferences";
